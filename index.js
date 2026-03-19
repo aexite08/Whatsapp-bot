96789a538c87e74b94c512b4808021301a21de23
aexite08 <aexite08@gmail.com>
2026-03-19 16:43:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-// Initialize the client
 const client = new Client({
-    puppeteer: { headless: true } // Removed executablePath
+    puppeteer: { headless: true } // cloud friendly
 });
 
-// Generate QR code in the Render logs
 client.on('qr', (qr) => {
     console.log('QR RECEIVED');
     qrcode.generate(qr, { small: true });
 });
 
-// Ready event
 client.on('ready', () => {
     console.log('Bot is ready!');
 });
 
-// Listen to incoming messages (example)
 client.on('message', message => {
     console.log(`Message from ${message.from}: ${message.body}`);
 });
 
-// Start the client
 client.initialize();
